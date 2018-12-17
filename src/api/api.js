a8c0dc19b5acfeb9268df3c78145a995de2a4aed
SZYZKJ <15622146998@163.com>
2018-12-18 02:55:44 +0800

    1.3.0

---
+++ src/api/api.js
@@ -0,0 +1,75 @@
+import {
+  wxRequestPost
+} from '@/utils/wxRequest';
+
+const apiMain = 'https://www.lianaizhuli.com/api/'
+
+//微信的jscode换取sessionKey
+const getOpenid = (params) => wxRequestPost(params, apiMain+'getOpenid');
+//判断是否存在openid
+const checkOpenid = (params) => wxRequestPost(params, apiMain+'checkOpenid');
+//获取手机号码
+const getPhoneNumber = (params) => wxRequestPost(params, apiMain+'getPhoneNumber');
+//搜索话术
+const searchHuashu = (params) => wxRequestPost(params, apiMain+'searchHuashu');
+//搜索惯例
+const searchGuanli = (params) => wxRequestPost(params, apiMain+'searchGuanli');
+//搜索表情
+const searchBiaoqing = (params) => wxRequestPost(params, apiMain+'searchBiaoqing');
+//获取理论
+const getMethodologyList = (params) => wxRequestPost(params, apiMain+'getMethodologyList');
+//获取关键字列表
+const getHiswordList = (params) => wxRequestPost(params, apiMain + 'getHiswordList');
+//获取关键字清除
+const clearHiswords = (params) => wxRequestPost(params, apiMain + 'clearHiswords');
+//获取热搜
+const getRecommend = (params) => wxRequestPost(params, apiMain + 'getRecommend');
+//获取文章列表
+const getWenzhangList = (params) => wxRequestPost(params, apiMain+'getWenzhangList');
+//获取视频列表
+const getGanhuoList = (params) => wxRequestPost(params, apiMain+'getGanhuoList');
+//获取课程列表
+const getKechengList = (params) => wxRequestPost(params, apiMain+'getKechengList');
+//搜索文章
+const searchWenzhangList = (params) => wxRequestPost(params, apiMain+'searchWenzhangList');
+//搜索视频
+const searchGanhuoList = (params) => wxRequestPost(params, apiMain+'searchGanhuoList');
+//获取prepay_id
+const get_prepay_id = (params) => wxRequestPost(params, apiMain + 'get_prepay_id');
+//获取特权
+const getTequan = (params) => wxRequestPost(params, apiMain + 'getTequan');
+//获取积分
+const getJifen = (params) => wxRequestPost(params, apiMain + 'getJifen');
+//获取订单
+const getDingdan = (params) => wxRequestPost(params, apiMain + 'getDingdan');
+//获取联盟
+const getIslianmeng = (params) => wxRequestPost(params, apiMain + 'getIslianmeng');
+//存储记录
+const setJilu = (params) => wxRequestPost(params, apiMain + 'setJilu');
+//获取广告列表
+const getAdList = (params) => wxRequestPost(params, apiMain + 'getAdList');
+
+export default {
+  getOpenid,
+  checkOpenid,
+  getPhoneNumber,
+  searchHuashu,
+  searchGuanli,
+  searchBiaoqing,
+  getMethodologyList,
+  getWenzhangList,
+  getGanhuoList,
+  getKechengList,
+  getHiswordList,
+  clearHiswords,
+  getRecommend,
+  searchWenzhangList,
+  searchGanhuoList,
+  get_prepay_id,
+  getTequan,
+  getJifen,
+  getDingdan,
+  getIslianmeng,
+  setJilu,
+  getAdList,
+}
---
+++ src/utils/wxRequest.js
@@ -0,0 +1,33 @@
+import wepy from 'wepy';
+var CryptoJS = require('./aes.js');
+var key = CryptoJS.enc.Utf8.parse("pangyuming920318");
+var iv = CryptoJS.enc.Utf8.parse("abcdefabcdefabcd");
+function Encrypt(word) {
+    var srcs = CryptoJS.enc.Utf8.parse(word);
+    var encrypted = CryptoJS.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+    return encrypted.ciphertext.toString();
+}
+function Decrypt(word) {
+    // var encryptedHexStr = CryptoJS.enc.Hex.parse(word);
+    // var srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+    var decrypt = CryptoJS.AES.decrypt(word, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+    var decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+    return decryptedStr.toString();
+}
+const wxRequestPost = async(params = {}, url) => {
+    let data = params.query || {};
+    var newdata=JSON.stringify(data);
+    newdata=Encrypt(newdata);
+    let res = await wepy.request({
+        url: url,
+        method: 'POST',
+        data: newdata,
+        header: { 'Content-Type': 'application/json' },
+    });
+    res.data=JSON.parse(Decrypt(res.data));
+    return res;
+};
+
+module.exports = {
+    wxRequestPost
+}
